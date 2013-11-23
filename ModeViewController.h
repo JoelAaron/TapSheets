a4c8d79e2815789a9b69fd171054168b56d99875
Sutton <jdsutton7464@jdsutton746m3.wireless.winona.edu>
2013-11-23 17:36:14 -0600

    removed settings button from main menu.  put settings menu as a modal segue between the main screen and the playviewcontroller.

---
+++ ModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModeViewController.h
+//  TapSheets
+//
+//  Created by Sutton, Joel D on 11/23/13.
+//  Copyright (c) 2013 Sutton, Joel D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModeViewController : UIViewController
+
+@end
---
+++ ModeViewController.m
@@ -0,0 +1,38 @@
+//
+//  ModeViewController.m
+//  TapSheets
+//
+//  Created by Sutton, Joel D on 11/23/13.
+//  Copyright (c) 2013 Sutton, Joel D. All rights reserved.
+//
+
+#import "ModeViewController.h"
+
+@interface ModeViewController ()
+
+@end
+
+@implementation ModeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
